dc222c07b4596cd4a151d72120cddf959a50be85
arneyva <princethereal1416@gmail.com>
2024-03-02 08:59:33 +0700

    koreksi unit dan currency

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -14,37 +14,29 @@ class UnitSeeder extends Seeder
     {
         Unit::firstOrCreate([
             'name' => 'gram',
-            'short_name' => 'g',
+            'ShortName' => 'g',
             'operator' => '*',
             'operator_value' => 1,
-            'description' => 'Base dari satuan berat',
-            'is_active' => 1,
         ]);
         Unit::firstOrCreate([
             'name' => 'Kilogram',
-            'short_name' => 'kg',
-            'base_unit_id' => 1,
+            'ShortName' => 'kg',
+            'base_unit' => 1,
             'operator' => '/',
-            'operator_value' => 0.001,
-            'description' => '1/1000 gram',
-            'is_active' => 1,
+            'operator_value' => 1000,
         ]);
         Unit::firstOrCreate([
             'name' => 'liter',
-            'short_name' => 'l',
+            'ShortName' => 'l',
             'operator' => '*',
             'operator_value' => 1,
-            'description' => 'Base dari satuan liter',
-            'is_active' => 1,
         ]);
         Unit::firstOrCreate([
             'name' => 'Mililiter',
-            'short_name' => 'ml',
-            'base_unit_id' => 3,
+            'ShortName' => 'ml',
+            'base_unit' => 3,
             'operator' => '*',
             'operator_value' => 1000,
-            'description' => '1000 liter',
-            'is_active' => 1,
         ]);
     }
 }
