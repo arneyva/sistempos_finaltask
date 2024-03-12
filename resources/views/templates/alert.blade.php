b8fbd1295ca695b1a2649f1d5947f97e80113349
arneyva <princethereal1416@gmail.com>
2024-03-12 14:18:46 +0700

    session create produk

--- resources/views/templates/alert.blade.php
+++ resources/views/templates/alert.blade.php
@@ -27,8 +27,8 @@
 <div style="text-align: center;" class="m-2">
     @if (\Session::has('success'))
         <div class="alert1 alert1-primary" role="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path fill="currentColor"
+            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+                <path fill="currentColor" style="margin-right: -20px"
                     d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm0 26a12 12 0 1 1 12-12a12 12 0 0 1-12 12Z" />
                 <path fill="currentColor" d="M15 8h2v11h-2zm1 14a1.5 1.5 0 1 0 1.5 1.5A1.5 1.5 0 0 0 16 22z" />
             </svg>
@@ -39,7 +39,7 @@
     @endif
     @if ($errors->any())
         <div class="alert1 alert1-danger" role="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">>
+            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
                 <path fill="currentColor" style="margin-right: -20px"
                     d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm0 26a12 12 0 1 1 12-12a12 12 0 0 1-12 12Z" />
                 <path fill="currentColor" d="M15 8h2v11h-2zm1 14a1.5 1.5 0 1 0 1.5 1.5A1.5 1.5 0 0 0 16 22z" />
