5b2baa1cbb1629584420692b17e880b9ebf49d0d
murof2003@student.uns.ac.id <Mujahid.murof2003@student.uns.ac.id>
2024-08-28 18:29:38 +0700

    p

--- database/migrations/2024_08_16_033829_alter_paymentpurch.php
+++ database/migrations/2024_08_16_033829_alter_paymentpurch.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,14 +13,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('payment_purchases', function (Blueprint $table) {
-            // Drop the existing foreign key constraint
-            $table->dropForeign(['user_id']);
 
             // Change the column to be nullable
             $table->integer('user_id')->nullable()->change();
 
-            // Re-add the foreign key constraint
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -32,13 +29,10 @@ return new class extends Migration
     {
         Schema::table('payment_purchases', function (Blueprint $table) {
             // Drop the existing foreign key constraint
-            $table->dropForeign(['user_id']);
 
             // Revert the column to be non-nullable
             $table->integer('user_id')->nullable(false)->change();
 
-            // Re-add the foreign key constraint
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 };
